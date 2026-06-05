1a6404ab7331d94d8877d3b41029e239247bc969
Assane <assane.diebate@etu.u-pec.fr>
2026-06-05 14:51:40 +0200

    Etape C: deuxième changement de la ligne demandé

--- hello.py
+++ hello.py
@@ -2,7 +2,8 @@ from colorama import init , Fore , Style
 init ()
 
 def display () :
-    message = f"{ Fore . GREEN } Bonjour les { Fore . CYAN } FI1{ Style . RESET_ALL }"
+    message = f"{ Fore . GREEN } Ceci est une { Fore . CYAN } modification { Style . RESET_ALL
+}"
     print ( message )
 if __name__ == " __main__ ":
     display ()
